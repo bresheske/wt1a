7d96b1229892c3453e8acaeedbddd9687d35384f
brandon resheske <bresheske@gmail.com>
2017-12-17 20:22:19 -0600

    scrolling to top

--- src/app/static/reveal.ts
+++ src/app/static/reveal.ts
@@ -1,6 +1,7 @@
 declare let window:any;
 export class Reveal {
     static reveal(selector:string) {
+        window.scrollTo(0,0);
         window.sr.reveal(selector, { duration: 200 }, 10);
     }
 }
